1076cda7ecf939ade128072f6377dddd3422afd2
Jonathan Eren <Jonathan.Eren@pinkerton.com>
2020-02-13 15:07:09 -0800

    AddEditUserFunctionality

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,7 +23,10 @@ import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -47,7 +50,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -70,7 +74,9 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       AuthService,
       ErrorInteceptorProvider,
       MemberDetailResolver,
+      MemberEditResolver,
       MemberListResolver,
+      PreventUnsavedChanges,
       // fix for gallery bug
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
