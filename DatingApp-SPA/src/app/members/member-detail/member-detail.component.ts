a95d688c51dd7f069b7ba4057ed90f22e593035f
Jonathan Eren <Jonathan.Eren@pinkerton.com>
2020-02-13 13:47:09 -0800

    AddMemberCardStyling

---
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+    }
+
+    return imageUrls;
+  }
+  // outdated as we now use resolvers, this is because without resolverers the route activates before the object
+  // is generated, we will now resolve the object and send that in via a resolver before generating the route,
+  // this will allow us to call the object in the html with out using ?
+
+  // comes in url as members/4 so we need the paramtere from router service
+  // adding the + before the params call to id converts it from a string to an int
+  // loadUser() {
+  //   this.userService.getUser(+this.route.snapshot.params.id).subscribe((user: User) => {
+  //     this.user = user;
+  //   }, error => {
+  //     this.alertify.error(error);
+  //   });
+  // }
+
+}
